0455788ef784a7212cd7665ed5ef501ce124fe85
Erick <Erick@Envydv4t5200>
2015-02-13 11:23:59 -0600

    Menu de creacion de plantas y listas para guardarlas.

--- src/plantasvszombis/MenuCreacionPlantas.java
+++ src/plantasvszombis/MenuCreacionPlantas.java
@@ -5,6 +5,9 @@
  */
 package plantasvszombis;
 
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Erick
@@ -16,6 +19,10 @@ public class MenuCreacionPlantas extends javax.swing.JFrame {
      */
     public MenuCreacionPlantas() {
         initComponents();
+        for(int x=1;x<100;x++){
+        lista_defensa.addItem(x);
+        lista_ataque.addItem(x);
+        }
     }
 
     /**
@@ -31,12 +38,12 @@ public class MenuCreacionPlantas extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        nombre_planta = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox();
+        lista_ataque = new javax.swing.JComboBox();
         jLabel6 = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox();
-        jButton1 = new javax.swing.JButton();
+        lista_defensa = new javax.swing.JComboBox();
+        boton_imagen = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         jComboBox3 = new javax.swing.JComboBox();
         jLabel1 = new javax.swing.JLabel();
@@ -56,41 +63,42 @@ public class MenuCreacionPlantas extends javax.swing.JFrame {
         jLabel3.setBackground(new java.awt.Color(153, 153, 255));
         jLabel3.setText("Su imagen Aquí :v");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(320, 20, 180, 110);
+        jLabel3.setBounds(320, 20, 180, 150);
 
         jLabel4.setText("Nombre");
         jPanel1.add(jLabel4);
         jLabel4.setBounds(10, 80, 70, 30);
-
-        jTextField1.setText("Ingrese El nombre de su planta");
-        jPanel1.add(jTextField1);
-        jTextField1.setBounds(120, 80, 160, 30);
+        jPanel1.add(nombre_planta);
+        nombre_planta.setBounds(120, 80, 160, 30);
 
         jLabel5.setText("Puntos de Ataque");
         jPanel1.add(jLabel5);
         jLabel5.setBounds(10, 120, 100, 30);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox1);
-        jComboBox1.setBounds(120, 130, 120, 20);
+        jPanel1.add(lista_ataque);
+        lista_ataque.setBounds(120, 130, 120, 20);
 
         jLabel6.setText("Puntos de Defensa");
         jPanel1.add(jLabel6);
         jLabel6.setBounds(10, 160, 100, 30);
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox2);
-        jComboBox2.setBounds(120, 170, 120, 20);
+        jPanel1.add(lista_defensa);
+        lista_defensa.setBounds(120, 170, 120, 20);
 
-        jButton1.setText("Elegir Imagen");
-        jPanel1.add(jButton1);
-        jButton1.setBounds(120, 20, 160, 40);
+        boton_imagen.setText("Elegir Imagen");
+        boton_imagen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                boton_imagenActionPerformed(evt);
+            }
+        });
+        jPanel1.add(boton_imagen);
+        boton_imagen.setBounds(120, 20, 160, 40);
 
         jLabel7.setText("Tipo de  Ataque");
         jPanel1.add(jLabel7);
         jLabel7.setBounds(10, 200, 100, 30);
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Disparador", "Ataque Directo", " " }));
+        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Disparador", "Ataque Directo" }));
         jPanel1.add(jComboBox3);
         jComboBox3.setBounds(120, 210, 120, 20);
 
@@ -108,6 +116,25 @@ public class MenuCreacionPlantas extends javax.swing.JFrame {
         setBounds(0, 0, 1091, 619);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void boton_imagenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boton_imagenActionPerformed
+        JFileChooser seleccion =new JFileChooser();
+        seleccion.showDialog(this, "Elija la imagen de su nueva planta");
+        if(PlantasvsZombis.jugadores.raiz==null){
+        mensaje("No ha creado ningun jugador");
+        }else if(PlantasvsZombis.jugadores.raiz.siguiente==null){
+                if(PlantasvsZombis.jugadores.raiz.tipo_jugador.equals("Planta")){
+                    mensaje("Debe crear al jugador zombi");
+                }else{
+                    mensaje("Debe crear al jugador Planta");
+                }
+    
+        
+        
+        }
+    }//GEN-LAST:event_boton_imagenActionPerformed
+    public void mensaje(String mensaje){
+    JOptionPane.showInternalMessageDialog(this, mensaje);
+    }
     /**
      * @param args the command line arguments
      */
@@ -145,9 +172,7 @@ public class MenuCreacionPlantas extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Mostrador;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JComboBox jComboBox2;
+    private javax.swing.JButton boton_imagen;
     private javax.swing.JComboBox jComboBox3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -157,6 +182,8 @@ public class MenuCreacionPlantas extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JComboBox lista_ataque;
+    private javax.swing.JComboBox lista_defensa;
+    private javax.swing.JTextField nombre_planta;
     // End of variables declaration//GEN-END:variables
 }
