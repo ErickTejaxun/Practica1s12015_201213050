221edf667e050d055be2b7669b0dd2847dffc219
Erick <Erick@Envydv4t5200>
2015-02-25 15:56:33 -0600

    Pilas y Filas graficadas

--- src/plantasvszombis/Graficas.java
+++ src/plantasvszombis/Graficas.java
@@ -5,6 +5,7 @@
  */
 package plantasvszombis;
 
+import Estructuras.Nodo;
 import Estructuras.nodo_atributo;
 import Estructuras.nodo_jugador;
 import java.awt.HeadlessException;
@@ -16,7 +17,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 /**
@@ -32,8 +32,13 @@ public String direccionBase;
      */
     public Graficas() {
         initComponents();
-        arboljugadores();
-}
+        
+        
+        
+            arboljugadores();
+            arbolespersonajes();
+           
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -47,6 +52,7 @@ public String direccionBase;
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         arbol_jugadores = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
         arbol_zombis = new javax.swing.JLabel();
@@ -59,10 +65,18 @@ public String direccionBase;
         jPanel5 = new javax.swing.JPanel();
         jPanel9 = new javax.swing.JPanel();
         arbol_personajes = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         arbol_jugadores.setText("Arbol de Jugadores");
+
+        jButton1.setText("ver");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -70,14 +84,19 @@ public String direccionBase;
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(arbol_jugadores, javax.swing.GroupLayout.PREFERRED_SIZE, 711, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 70, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(arbol_jugadores, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addGap(0, 32, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Jugadores", jPanel1);
@@ -90,7 +109,7 @@ public String direccionBase;
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel6Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(arbol_zombis, javax.swing.GroupLayout.DEFAULT_SIZE, 741, Short.MAX_VALUE)
+                .addComponent(arbol_zombis, javax.swing.GroupLayout.DEFAULT_SIZE, 748, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel6Layout.setVerticalGroup(
@@ -165,7 +184,7 @@ public String direccionBase;
             jPanel8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel8Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(arbol_matriz, javax.swing.GroupLayout.DEFAULT_SIZE, 741, Short.MAX_VALUE)
+                .addComponent(arbol_matriz, javax.swing.GroupLayout.DEFAULT_SIZE, 748, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel8Layout.setVerticalGroup(
@@ -203,14 +222,14 @@ public String direccionBase;
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel9Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(arbol_personajes, javax.swing.GroupLayout.DEFAULT_SIZE, 741, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(arbol_personajes, javax.swing.GroupLayout.PREFERRED_SIZE, 699, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(52, Short.MAX_VALUE))
         );
         jPanel9Layout.setVerticalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel9Layout.createSequentialGroup()
                 .addGap(19, 19, 19)
-                .addComponent(arbol_personajes, javax.swing.GroupLayout.DEFAULT_SIZE, 420, Short.MAX_VALUE)
+                .addComponent(arbol_personajes, javax.swing.GroupLayout.DEFAULT_SIZE, 439, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -228,18 +247,29 @@ public String direccionBase;
             .addGroup(jPanel5Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(24, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Personajes", jPanel5);
+
+        jButton2.setText("Ver Grafica");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jTabbedPane1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton2))
+                    .addComponent(jTabbedPane1))
                 .addGap(25, 25, 25))
         );
         layout.setVerticalGroup(
@@ -247,11 +277,21 @@ public String direccionBase;
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 513, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(25, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+ imprimir1();        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+imprimir2();        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -287,6 +327,16 @@ public String direccionBase;
             }
         });
     }
+    public void imprimir1(){
+                    ImageIcon imagen = new ImageIcon(direccionBase+"\\jugadores.png");
+                    ImageIcon imagenredimencionada= new ImageIcon(imagen.getImage().getScaledInstance(arbol_jugadores.getWidth(), arbol_jugadores.getHeight(),0));
+                    arbol_jugadores.setIcon(imagenredimencionada);
+    }
+        public void imprimir2(){
+                    ImageIcon imagen = new ImageIcon(direccionBase+"\\arbolpersonajes.png");
+                    ImageIcon imagenredimencionada= new ImageIcon(imagen.getImage().getScaledInstance(arbol_personajes.getWidth(), arbol_personajes.getHeight(),0));
+                    arbol_personajes.setIcon(imagenredimencionada);
+    }
 
     private void arboljugadores(){
         String texto="";
@@ -312,7 +362,7 @@ public String direccionBase;
                     nodo_jugador jugadores=PlantasvsZombis.jugadores.raiz;
                     nodo_atributo atributos=jugadores.atributos.raiz;
                         //Recolectamos los datos del primer jugador
-                        int numeronodo=2;                       
+                        int numeronodo=3;                       
                         texto+="digraph G{ \n" +
                         " rankidir = LR;\n" +
                         " node[shape = record]; \n" +
@@ -322,12 +372,13 @@ public String direccionBase;
                         "nodo0[label=\""+PlantasvsZombis.jugadores.raiz.nombre_jugador+"\"];\n" +
                         "nodo1[label=\""+PlantasvsZombis.jugadores.raiz.numero_personajes+"\"];";
                          while(atributos!=null){
-                         texto+="nodo"+numeronodo+"[label=\""+atributos.nombre+"\"];\n"+
-                                 "nodo"+numeronodo+"[label=\""+atributos.dato+"\"];\n";
+                         texto+="nodo"+numeronodo+"[label=\""+atributos.nombre+"\"];\n";
+                                 numeronodo++;
+                         texto+="nodo"+numeronodo+"[label=\""+atributos.dato+"\"];\n";
                          atributos=atributos.siguiente;
                          numeronodo++;
                          }
-                         texto+= PlantasvsZombis.jugadores.raiz.tipo_jugador+" -> nodo0";
+                         texto+= PlantasvsZombis.jugadores.raiz.tipo_jugador+" -> nodo0;";
                          for(int g=0;g<numeronodo;g++)
                          {
                              texto+=" nodo"+g+" -> nodo"+(g+1)+";";
@@ -340,15 +391,16 @@ public String direccionBase;
                         "nodos0[label=\""+PlantasvsZombis.jugadores.ultimo.nombre_jugador+"\"];\n" +
                         "nodos1[label=\""+PlantasvsZombis.jugadores.ultimo.numero_personajes+"\"];";
                          while(atributos2!=null){
-                         texto+="nodos"+numeronodo+"[label=\""+atributos2.nombre+"\"];\n"+
-                                 "nodos"+numeronodo+"[label=\""+atributos2.dato+"\"];\n";
+                         texto+="nodos"+numeronodo+"[label=\""+atributos2.nombre+"\"];\n";
+                         numeronodo++;
+                         texto+="nodos"+numeronodo+"[label=\""+atributos2.dato+"\"];\n";
                          atributos2=atributos2.siguiente;
                          numeronodo++;
                          }
-                         texto+= PlantasvsZombis.jugadores.ultimo.tipo_jugador+" -> nodos0";
+                         texto+= PlantasvsZombis.jugadores.ultimo.tipo_jugador+" -> nodos0;";
                          for(int g=0;g<numeronodo;g++)
                          {
-                             texto+=" nodos"+g+" -> nodos"+(g+1)+";";
+                             texto+="nodos"+g+" -> nodos"+(g+1)+";";
                          }
                          texto+="}; Jugadores -> "+PlantasvsZombis.jugadores.raiz.tipo_jugador+"; \n" +
                                     PlantasvsZombis.jugadores.raiz.tipo_jugador+" -> "+PlantasvsZombis.jugadores.ultimo.tipo_jugador+"; \n" +
@@ -363,9 +415,7 @@ public String direccionBase;
                     generarArchivo(texto,"jugadores");
                     GenerarArbol("jugadores");
                     
-                    ImageIcon imagen = new ImageIcon(direccionBase+"\\jugadores.png");
-                    ImageIcon imagenredimencionada= new ImageIcon(imagen.getImage().getScaledInstance(400, 400,0));
-                    arbol_jugadores.setIcon(imagenredimencionada);
+                    
                     
                 } catch (FileNotFoundException ex) {
                     
@@ -377,6 +427,92 @@ public String direccionBase;
         
     }
 public void arbolespersonajes(){
+      String texto="";
+      String auxiliar="";
+        path="";
+        direccionBase = "C:\\Users\\Erick\\Documents\\NetBeansProjects\\PlantasvsZombis\\Graficas\\";
+        
+            
+            path=direccionBase+"\\arbolpersonajes.txt";
+            try{
+                FileWriter nuevo=new FileWriter(path);
+                nuevo.close();
+            }catch(IOException | HeadlessException e){
+                System.out.println(e);
+            }
+            File abrir=new File(path);
+            
+                FileReader lector;
+                try{
+                    lector=new FileReader(abrir);
+                    BufferedReader lee = new BufferedReader(lector);
+                    String aux="";
+                    Nodo plantas=(Nodo) PlantasvsZombis.Lista_plantas.raiz.contenido;
+                                            //Recolectamos los datos del primer jugador
+                        int numeronodo=0;                       
+                        texto+="digraph G{ \n" +
+                                " rankidir = LR;\n" +
+                                " node[shape = circle]; \n" +
+                                " rankidir = UD;\n" +
+                                " {  rank = same; \n" +
+                                "Personajes;"
+                                + "Plantas ->";
+                        
+                        
+                        //       Planta -> Zombie; 
+                        //       Zombie -> Zombie2;
+                        //        Zombie2 -> Zombie3;                 
+                        while(plantas!=null){
+                        texto+=plantas.nombre+"; \n "+plantas.nombre+" -> ";
+                        plantas=plantas.siguiente;
+                        }
+                        
+                         texto+=plantas.nombre+"; \n";
+                         plantas=PlantasvsZombis.Lista_plantas.raiz;
+                         while(plantas!=null){
+                         texto+=plantas.nombre+"[label=\"Nombre "+plantas.nombre+"\"];\n";
+                         numeronodo++;
+                         texto+=numeronodo+"nodo"+numeronodo+"[label=\"Ataque "+plantas.ataque+"\"];\n";
+                         numeronodo++;
+                         texto+=numeronodo+"nodo"+numeronodo+"[label=\"Defensa "+plantas.defensa+"\"];\n";
+                         numeronodo++;
+                         texto+=numeronodo+"nodo"+numeronodo+"[label=\"Tipo "+plantas.tipo+"\"];\n";
+                         numeronodo++;
+                         texto+=numeronodo+"nodo"+numeronodo+"[label=\"Path "+plantas.pathimagen+"\"];\n";
+                         plantas=plantas.siguiente;
+                         numeronodo++;
+                         texto+= PlantasvsZombis.jugadores.raiz.tipo_jugador+" -> nodo0;";
+                         for(int g=0;g<numeronodo;g++)
+                         {
+                             texto+=numeronodo+"nodo"+g+" -> "+numeronodo+"nodo"+(g+1)+";";
+                         }
+                         
+                         }                       
+                         texto+="};\n" +
+                                    " \n" +
+                                    "; }\n" +
+                                    " } ";
+                         
+                         
+                    
+                    lee.close();
+                    
+                    generarArchivo(texto,"arbolpersonajes");
+                    GenerarArbol("arbolpersonajes");
+                    
+                    
+                    
+                } catch (FileNotFoundException ex) {
+                    
+                } catch (IOException ex) {
+                    
+                }
+            
+            
+        
+    
+    
+    
 }
 
 public void GenerarArbol(String nombrearchivo)
@@ -413,6 +549,8 @@ public void GenerarArbol(String nombrearchivo)
     private javax.swing.JLabel arbol_personajes;
     private javax.swing.JLabel arbol_plantas;
     private javax.swing.JLabel arbol_zombis;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
